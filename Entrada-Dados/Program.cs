87c9caf2f4f078bdb8736aada831109c3fb92f9a
Brenno-Daniel <brenno.daniel.98@gmail.com>
2018-08-12 14:29:37 -0300

    commitando codigos de estudos

---
+++ Entrada-Dados/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Entrada_Dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Soma de valores digitados pelo usuário
+            string nome;
+            double num1, num2, res;
+
+            Console.WriteLine("Digite o nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            res = num1 + num2;
+
+            Console.WriteLine(nome + " soma dos valores é: " + res);
+            
+            
+
+
+
+        }
+    }
+}
